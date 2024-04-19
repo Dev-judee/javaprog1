5e2361bb04136e20b4f08fa37baf1ab61f7780b1
Judecaindoy <cndyjd@gamil.com>
2024-04-19 17:33:50 +0800

    added course to the code

--- prog1.java
+++ prog1.java
@@ -1,5 +1,11 @@
 public class Prog1{
 	public static void main (String [] args){
 		System.out.println("PATEROS TECHNOLOGICAL COLLEGE");
+		System.out.println(" TECHNOLOGICALE");
+		System.out.println("COLLEGE");
+		System.out.println("COURSE BSCS");
+}
+}
+}
 }
 }
